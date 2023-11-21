27a3f3905898a936bba078cb03d5f7fd5dafb446
ChloeL-E <chloe_c123@hotmail.co.uk>
2023-11-21 11:14:45 +0000

    Improved user accessibility by changing all hero headings to h2 as per WAVE tool recommendations

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-/*const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav ul");
 
-hamburger.addEventListener("click", () => {
-    console.log("Hamburger click");
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav ul").forEach(n => n.addEventListener("click", () => {
-  console.log("Document click");
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))*/
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
